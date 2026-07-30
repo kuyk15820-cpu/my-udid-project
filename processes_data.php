b7dd511bf2343135d63307ef40b0f86c5786d52f
kuyk15820-cpu <kuyk15820@gmail.com>
2026-07-31 02:40:28 +0700

    Update processes_data.php

--- processes_data.php
+++ processes_data.php
@@ -1,53 +1,73 @@
 <?php
-//get data
+// ดึงข้อมูล Payload ที่ iOS ส่งมา
 $data = file_get_contents('php://input');
 
-//extract XML from data
-$plistBegin   = '<?xml version="1.0"';
-$plistEnd   = '</plist>';
+if (!empty($data)) {
+    // ตัดเอาเฉพาะส่วนที่เป็น XML
+    $plistBegin = '<?xml version="1.0"';
+    $plistEnd   = '</plist>';
 
-$pos1 = strpos($data, $plistBegin);
-$pos2 = strpos($data, $plistEnd);
-$data2 = substr ($data,$pos1,$pos2-$pos1);
+    $pos1 = strpos($data, $plistBegin);
+    $pos2 = strpos($data, $plistEnd);
 
+    if ($pos1 !== false && $pos2 !== false) {
+        $data2 = substr($data, $pos1, ($pos2 - $pos1) + 8);
+        $xml = simplexml_load_string($data2);
+        
+        $UDID = "";
+        $DEVICE_PRODUCT = "";
+        $DEVICE_VERSION = "";
+        $SERIAL = "";
+        $IMEI = "";
 
-$xml = xml_parser_create();
-xml_parse_into_struct($xml, $data2, $vs);
-xml_parser_free($xml);
+        if ($xml !== false) {
+            $nodes = $xml->dict->children();
+            $key = null;
+            
+            // วนลูปอ่านค่า Key ต่างๆ จาก XML
+            foreach ($nodes as $node) {
+                if ($node->getName() == 'key') {
+                    $key = (string)$node;
+                } elseif ($node->getName() == 'string' && $key !== null) {
+                    switch ($key) {
+                        case 'UDID':
+                            $UDID = (string)$node;
+                            break;
+                        case 'PRODUCT':
+                            $DEVICE_PRODUCT = (string)$node;
+                            break;
+                        case 'VERSION':
+                            $DEVICE_VERSION = (string)$node;
+                            break;
+                        case 'SERIAL':
+                            $SERIAL = (string)$node;
+                            break;
+                        case 'IMEI':
+                            $IMEI = (string)$node;
+                            break;
+                    }
+                    $key = null;
+                }
+            }
+        }
 
-$UDID = "";
-$DEVICE_PRODUCT = "";
-$DEVICE_VERSION = "";
-$DEVICE_NAME = "";
-$iterator = 0;
+        // รวมรหัสโมเดลตรงๆ (เช่น iPhone10,3) เข้ากับ Version (เช่น 20D67)
+        $product_version = $DEVICE_PRODUCT;
+        if (!empty($DEVICE_VERSION)) {
+            $product_version .= " / " . $DEVICE_VERSION;
+        }
 
-$arrayCleaned = array();
-foreach($vs as $v){
+        // ส่ง Parameter ต่อไปที่หน้า show_detail.php
+        $params = "UDID=" . urlencode($UDID) .
+                  "&IMEI=" . urlencode($IMEI) .
+                  "&PRODUCT_VERSION=" . urlencode($product_version) .
+                  "&SERIAL=" . urlencode($SERIAL);
 
-    if($v['level'] == 3 && $v['type'] == 'complete'){
-        $arrayCleaned[]= $v;
+        header("Location: show_detail.php?" . $params, true, 301);
+        exit();
     }
-    $iterator++;
 }
 
-$iterator = 0;
-foreach($arrayCleaned as $elem){
-    switch ($elem['value']) {
-        case "UDID":
-            $UDID = $arrayCleaned[$iterator+1]['value'];
-            break;
-        case "PRODUCT":
-            $DEVICE_PRODUCT = $arrayCleaned[$iterator+1]['value'];
-            break;
-        case "VERSION":
-            $DEVICE_VERSION = $arrayCleaned[$iterator+1]['value'];
-            break;
-        case "DEVICE_NAME":
-            $DEVICE_NAME = $arrayCleaned[$iterator+1]['value'];
-            break;
-    }
-    $iterator++;
-}
-
-$params = "UDID=".$UDID."&DEVICE_PRODUCT=".$DEVICE_PRODUCT."&DEVICE_VERSION=".$DEVICE_VERSION."&DEVICE_NAME=".$DEVICE_NAME;
-header("Location: show_detail.php?".$params,TRUE,301);
+// ถ้าไม่มีข้อมูล ให้ Redirect กลับหน้าแสดงผลเปล่าๆ
+header("Location: show_detail.php", true, 301);
+exit();
