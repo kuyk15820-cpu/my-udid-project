d357f30c59a549d4c67cf36dd1aa0fbffc139e8f
kuyk15820-cpu <kuyk15820@gmail.com>
2026-07-31 22:37:10 +0700

    Create check_status.php

---
+++ check_status.php
@@ -0,0 +1,11 @@
+<?php
+$flag_file = __DIR__ . '/maintenance_flag.json';
+
+if (file_exists($flag_file)) {
+    $data = json_decode(file_get_contents($flag_file), true);
+    if (isset($data['maintenance']) && $data['maintenance'] === true) {
+        header('Location: maintenance.html');
+        exit;
+    }
+}
+?>
