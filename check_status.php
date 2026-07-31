62ff18c8bc90fa0077df45cde759396edb1e3427
kuyk15820-cpu <kuyk15820@gmail.com>
2026-07-31 22:47:31 +0700

    Update check_status.php

--- check_status.php
+++ check_status.php
@@ -4,7 +4,7 @@ $flag_file = __DIR__ . '/maintenance_flag.json';
 if (file_exists($flag_file)) {
     $data = json_decode(file_get_contents($flag_file), true);
     if (isset($data['maintenance']) && $data['maintenance'] === true) {
-        header('Location: maintenance.html');
+        header('Location: maintenance.php');
         exit;
     }
 }
